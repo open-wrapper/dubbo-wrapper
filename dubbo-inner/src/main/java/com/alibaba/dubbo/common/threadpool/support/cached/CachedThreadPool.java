c08e96acb37e101feece289a79363f2f1fea72d4
codeL <576579140@qq.com>
2020-04-20 09:12:55 +0800

    delete

--- dubbo-inner/src/main/java/com/alibaba/dubbo/common/threadpool/support/cached/CachedThreadPool.java
+++
@@ -1,9 +0,0 @@
-package com.alibaba.dubbo.common.threadpool.support.cached;
-
-/**
- * @author codel
- * @since 2020-01-16
- */
-@Deprecated
-public class CachedThreadPool extends  org.apache.dubbo.common.threadpool.support.cached.CachedThreadPool {
-}
